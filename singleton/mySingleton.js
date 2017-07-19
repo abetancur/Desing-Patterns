a298fcb3bd925e6c17b139418b1cee7fe2d271fc
alejandro <alejo@hotmail>
2017-07-19 10:51:55 -0500

    Singleton Pattern

---
+++ singleton/mySingleton.js
@@ -0,0 +1,21 @@
+var mySingleton = (function () {
+    var instance;
+    function init() {
+        var instance = new Object("Task");
+        return instance;
+    }
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = init();
+                console.log('Create new instance');
+                return instance;
+            }
+            console.log('Return instance');
+            return instance;
+        }
+    }
+})();
+
+var singleA = mySingleton.getInstance();
+var singleB = mySingleton.getInstance();
